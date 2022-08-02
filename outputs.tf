0b786b0343015abe745100f2afa5b1246dedfcc4
Matt McElreath <matt.mcelreath@gmail.com>
2022-08-02 11:42:34 -0400

    Added description to workspace output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "workspace" {
   value = tfe_workspace.workspace
+  description = "Outputs the values returned by the tfe_workspace resource. https://registry.terraform.io/providers/hashicorp/tfe/latest/docs/resources/workspace"
 }
